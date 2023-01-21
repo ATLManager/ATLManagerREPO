67fe1fe74bc6c75693c3f557ddc4ce44b33e850a
ZenKha <bruno.resende24@gmail.com>
2023-01-21 20:57:29 +0000

    Projeto Base
    Base do projeto - default do VS

---
+++ ATLManager/ATLManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATLManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
