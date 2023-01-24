4a21269a70c5e41922e44b40b871a1aaaada6779
ZenKha <bruno.resende24@gmail.com>
2023-01-24 15:46:31 +0000

    Login e Register Base

---
+++ ATLManager/ATLManager/Areas/Identity/Data/ATLManagerUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ATLManager.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ATLManagerUser class
+public class ATLManagerUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+    
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(15)")]
+    public string CC { get; set; } = string.Empty;
+
+    public ATLManagerUserRole Role { get; set; }
+
+    public ATLManagerUserStatus Status { get; set; }
+}
+
--- ATLManager/ATLManager/obj/Debug/net6.0/ATLManager.AssemblyInfo.cs
+++ ATLManager/ATLManager/obj/Debug/net6.0/ATLManager.AssemblyInfo.cs
@@ -12,7 +12,6 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-ATLManager-216855b3-7397-4170-9cfe-79c0c5e7cab0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATLManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
