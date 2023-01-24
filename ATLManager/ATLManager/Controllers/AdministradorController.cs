2e8e84859bdaae67f8449b88051cf373872f8e08
Pedro Silva <pedrolinomorgado@hotmail.com>
2023-01-24 21:26:31 +0000

    Creation of the base Controllers again

---
+++ ATLManager/ATLManager/Controllers/AdministradorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATLManager.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
