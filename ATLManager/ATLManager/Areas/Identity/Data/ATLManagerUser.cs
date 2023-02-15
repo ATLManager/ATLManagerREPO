7ec7e91606fdd5c2e4c4f9ddc46cec13044ac322
ZenKha <bruno.resende24@gmail.com>
2023-02-15 22:06:08 +0000

    Validação Email
    Em ATLManagerUser. Foi posto uma verificação regex no atributo Email

--- ATLManager/ATLManager/Areas/Identity/Data/ATLManagerUser.cs
+++ ATLManager/ATLManager/Areas/Identity/Data/ATLManagerUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,15 +11,24 @@ namespace ATLManager.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ATLManagerUser class
 public class ATLManagerUser : IdentityUser
 {
-    [PersonalData]
+    [ProtectedPersonalData]
     [Column(TypeName = "nvarchar(100)")]
+    [Required]
     public string FirstName { get; set; }
     
-    [PersonalData]
+    [ProtectedPersonalData]
     [Column(TypeName = "nvarchar(100)")]
+    [Required]
     public string LastName { get; set; }
 
-    [PersonalData]
+    [Required]
+    [EmailAddress]
+    [ProtectedPersonalData]
+    [RegularExpression(@"^[a-zA - Z0 - 9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$)", 
+        ErrorMessage = "Email Inválido")]
+    override public string Email { get; set; }
+
+    [ProtectedPersonalData]
     [Column(TypeName = "nvarchar(15)")]
     public string CC { get; set; } = string.Empty;
 
