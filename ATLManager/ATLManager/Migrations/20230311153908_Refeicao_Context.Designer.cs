65886608eb62c93fe36e896f70680af2a37a4e87
filipecolaco <80459127+filipecolaco@users.noreply.github.com>
2023-03-11 16:33:56 +0000

    CRUD Refeições
    Crud de refeições

--- ATLManager/ATLManager/Migrations/20230311153908_Refeicao_Context.Designer.cs
+++ ATLManager/ATLManager/Migrations/20230311153908_Refeicao_Context.Designer.cs
@@ -4,6 +4,7 @@ using ATLManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ATLManager.Migrations
 {
     [DbContext(typeof(ATLManagerAuthContext))]
-    partial class ATLManagerAuthContextModelSnapshot : ModelSnapshot
+    [Migration("20230311153908_Refeicao_Context")]
+    partial class Refeicao_Context
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -228,55 +230,48 @@ namespace ATLManager.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<string>("AGSat")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("AGSat")
+                        .HasColumnType("real");
 
-                    b.Property<string>("Acucar")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("Acucar")
+                        .HasColumnType("real");
 
                     b.Property<string>("Categoria")
                         .IsRequired()
                         .HasMaxLength(20)
                         .HasColumnType("nvarchar(20)");
 
-                    b.Property<DateTime>("Data")
-                        .HasColumnType("datetime2");
+                    b.Property<string>("Data")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Descricao")
                         .IsRequired()
                         .HasMaxLength(255)
                         .HasColumnType("nvarchar(255)");
 
-                    b.Property<string>("HidratosCarbono")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("HidratosCarbono")
+                        .HasColumnType("real");
 
-                    b.Property<string>("Lipidos")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("Lipidos")
+                        .HasColumnType("real");
 
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(20)
                         .HasColumnType("nvarchar(20)");
 
-                    b.Property<string>("Proteina")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("Proteina")
+                        .HasColumnType("real");
 
-                    b.Property<string>("Sal")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("Sal")
+                        .HasColumnType("real");
 
-                    b.Property<string>("VR")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("VR")
+                        .HasColumnType("real");
 
-                    b.Property<string>("ValorEnergetico")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<float>("ValorEnergetico")
+                        .HasColumnType("real");
 
                     b.HasKey("RefeicaoId");
 
---
+++ ATLManager/ATLManager/Migrations/20230311153908_Refeicao_Context.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATLManager.Migrations
+{
+    public partial class Refeicao_Context : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Refeicao",
+                columns: table => new
+                {
+                    RefeicaoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Categoria = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Proteina = table.Column<float>(type: "real", nullable: false),
+                    HidratosCarbono = table.Column<float>(type: "real", nullable: false),
+                    VR = table.Column<float>(type: "real", nullable: false),
+                    Acucar = table.Column<float>(type: "real", nullable: false),
+                    Lipidos = table.Column<float>(type: "real", nullable: false),
+                    ValorEnergetico = table.Column<float>(type: "real", nullable: false),
+                    AGSat = table.Column<float>(type: "real", nullable: false),
+                    Sal = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Refeicao", x => x.RefeicaoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Refeicao");
+        }
+    }
+}
