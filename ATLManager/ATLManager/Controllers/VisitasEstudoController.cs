ceb389c83d71089fb32bb5c97f9ccdf599fee128
filipecolaco <80459127+filipecolaco@users.noreply.github.com>
2023-04-22 04:12:45 +0100

    BugFix VisitaEstudo Edit data

--- ATLManager/ATLManager/Controllers/VisitasEstudoController.cs
+++ ATLManager/ATLManager/Controllers/VisitasEstudoController.cs
@@ -184,6 +184,17 @@ namespace ATLManager.Controllers
                 return NotFound();
             }
 
+
+            DateTime dataAtual = DateTime.Now;
+
+            DateTime dataViewModel = DateTime.Parse(viewModel.Date);
+            if (dataViewModel.CompareTo(dataAtual) < 0)
+            {
+                var validationMessage = "Não é possível criar uma Visita de Estudo com uma data anterior à data atual";
+                ModelState.AddModelError("Date", validationMessage);
+            }
+
+
             if (ModelState.IsValid)
             {
                 try
